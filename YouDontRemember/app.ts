59102578f51a98402bfe1e28336da02a45269cb5
BlackEspresso <sweet.black.espresso@googlemail.com>
2016-02-18 22:01:07 +0100

    add game logic

--- YouDontRemember/app.ts
+++ YouDontRemember/app.ts
@@ -1,6 +1,9 @@
-﻿window.onload = startGame;
+﻿var startLevel = 3;
 
-function startGame() {
+window.onload = () => startGame(startLevel);
+
+function startGame(level: number) {
+    var checkedClicked = false;
     var wg = new WordGenerator();
     wg.syllable = ['EN', 'ER', 'CH', 'DE', 'EI', 'IE', 'IN', 'TE',
         'GE', 'UN', 'ND', 'IC', 'ES', 'BE', 'HE', 'ST', 'NE', 'AN',
@@ -8,13 +11,18 @@ function startGame() {
         'HT', 'EL', 'LI'];
 
     var screens = ['screenNewWords', 'screenPickWords'];
+    $('#levelNumber').text(level - startLevel + 1);
 
     changeScreen(screens, screens[0]);
 
-    var words = wg.genWords(5);
-    showWordList('wordlist', words);
+    var words = wg.genWords(level);
+    console.log(words);
+    var list = $('#wordlist');
+    list.children().remove();
+    showWordList(list, words, (el) => el.addClass('word'));
 
     var timer = new Timer();
+    timer.duration = 10 + level - startLevel;
     timer.updateFunc = (timer) => {
         $('#timer').text(timer.count);
         if(timer.isDone)
@@ -22,26 +30,104 @@ function startGame() {
     }
     timer.start();
 
-    var allWords = wg.genWords(15);
+    var allWords = wg.genWords(level*3);
     for (var x = 0; x < words.length; x++) {
         var i = Math.floor(Math.random() * allWords.length);
         allWords.splice(i, 0, words[x]);
     }
-    showWordList('pickWordlist', allWords);
-}
 
-function changeScreen(screens: string[], name: string) {
-    for (var x = 0; x < screens.length; x++) {
-        $('#' + screens[x]).hide();
+    var pickWordList = $('#pickWordlist');
+    pickWordList.children().remove();
+    showWordList(pickWordList, allWords, (el) => {
+        el.addClass('clickable-word')
+        el.click(onWordClick);
+    });
+    onWordClick(null);
+    checkedClicked = false;
+
+    var btnCheckWords = $('#btnCheckWords');
+
+    $('#titlePickWords').show();
+    $('#titleYouWon').hide();
+    $('#titleYouLoose').hide();
+    $('#btnNextLevel').hide();
+    $('#btnTryAgain').hide();
+    $('#btnNextLevel').hide();
+
+    btnCheckWords.one('click',() => {
+        var list = $('#pickWordlist');
+        var allCorrect = true;
+        checkedClicked = true;
+        btnCheckWords.hide();
+
+        list.find('.selected').each((i, e) => {
+            var el = $(e);
+            var word = el.text();
+            var correct = words.indexOf(word) > -1;
+
+            if (correct) {
+                el.addClass('correct-word')
+            } else {
+                el.addClass('wrong-word');
+                allCorrect = false;
+            }
+           
+        });
+
+        $('#titlePickWords').hide();
+        if (allCorrect) {
+            $('#titleYouWon').show();
+            $('#btnNextLevel').show();
+            $('#btnNextLevel').one('click', () => {
+                startGame(level + 1);
+            });
+        }
+        else {
+            $('#titleYouLoose').show();
+            $('#btnTryAgain').show();
+            $('#btnTryAgain').one('click', () => {
+                startGame(3);
+            });
+        }
+
+    });
+   
+
+
+    function onWordClick(e: JQueryEventObject) {
+        if (checkedClicked)
+            return;
+        var target = e != null ? $(e.target) : $('<div>');
+        var parent = $('#screenPickWords');
+        var picked = parent.find('.selected').length;
+        var maxCount = level;
+        if (picked < maxCount || target.hasClass('selected')) {
+            target.toggleClass('selected');
+            picked = parent.find('.selected').length;
+        }
+        if (picked == maxCount) {
+            $('#btnCheckWords').show();
+        } else {
+            $('#btnCheckWords').hide();
+        }
+        $('#pickCount').text(picked);
+        $('#maxPickable').text(maxCount);
     }
-    $('#' + name).show();
-}
 
-function showWordList(id, words) {
-    var start = $('#'+id);
-    for (var x = 0; x < words.length; x++) {
-        var el = $('<div>').text(words[x]);
-        start.append(el);
+    function changeScreen(screens: string[], name: string) {
+        for (var x = 0; x < screens.length; x++) {
+            $('#' + screens[x]).hide();
+        }
+        $('#' + name).show();
+    }
+
+    function showWordList(listEl, words, func: (el: JQuery) => void) {
+        for (var x = 0; x < words.length; x++) {
+            var el = $('<div>').text(words[x]);
+            if (func != null)
+                func(el);
+            listEl.append(el);
+        }
     }
 }
 
